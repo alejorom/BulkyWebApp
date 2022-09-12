59beb1130463d6b71371eb24bd48e4054107738b
alejorom <94083673+alejorom@users.noreply.github.com>
2022-09-12 11:04:24 -0500

    Adicionar DBContext Settings

--- AbbyWeb/Abby.DataAccess/Data/ApplicationDbContext.cs
+++ AbbyWeb/Abby.DataAccess/Data/ApplicationDbContext.cs
@@ -5,6 +5,11 @@ namespace AbbyWeb.Abby.DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        } 
+
         public DbSet<Category> Category { get; set; }
 
     }
