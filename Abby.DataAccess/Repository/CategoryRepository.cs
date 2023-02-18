a99c50421a05ddc3a28bd3f7face71004dc2c20f
alejorom <94083673+alejorom@users.noreply.github.com>
2023-02-18 16:53:32 -0500

    Adicionar Categoria Repository

---
+++ Abby.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Abby.DataAccess.Data;
+using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
+
+namespace Abby.DataAccess.Repository
+{
+	public class CategoryRepository : Repository<Category>, ICategoryRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CategoryRepository(ApplicationDbContext context) : base(context)
+		{
+			_context = context;	
+		}
+
+		public void Update(Category category)
+		{
+			var objFromDb = _context.Category.FirstOrDefault(u => u.Id == category.Id);
+			objFromDb.Name = category.Name;
+			objFromDb.DisplayOrder = category.DisplayOrder;
+		}
+
+		public void Save()
+		{
+			_context.SaveChanges();
+		}
+
+	}
+}
