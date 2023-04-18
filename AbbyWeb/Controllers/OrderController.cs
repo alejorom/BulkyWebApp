b65deb7dd641c488ff542d18e900d0d1622e1c8f
alejorom <94083673+alejorom@users.noreply.github.com>
2023-04-18 15:33:54 -0500

    Adicionar OrderList

---
+++ AbbyWeb/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Abby.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbbyWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+			var OrderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+			return Json(new { data = OrderHeaderList });
+		}
+
+    }
+}
---
+++ AbbyWeb/wwwroot/js/orderList.js
@@ -0,0 +1,29 @@
+﻿var dataTable;
+$(document).ready(function () {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/order",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "id", "width": "15%" },
+            { "data": "pickupName", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            { "data": "pickUpTime", "width": "25%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" >
+                            <a href="/Admin/Order/OrderDetails?id=${data}"  class="btn btn-success text-white mx-2">
+                            <i class="bi bi-pencil-square"></i>  </a>
+                            </div>`
+                },
+
+                "width": "15%"
+            }
+        ],
+        "width": "100%"
+    });
+});
